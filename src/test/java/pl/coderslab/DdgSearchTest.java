6fb97a65edbb7afd465ed728e00706b6feb5547d
Rafal <rafal.opial@coderslab.pl>
2022-02-27 12:10:52 +0100

    first test - search with duckduckgo part 2

--- src/test/java/pl/coderslab/DdgSearchTest.java
+++ src/test/java/pl/coderslab/DdgSearchTest.java
@@ -1,7 +1,9 @@
 package pl.coderslab;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DdgSearchTest {
@@ -10,6 +12,10 @@ public class DdgSearchTest {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://duckduckgo.com/");
+        WebElement searchInputBox = driver.findElement(By.id("search_form_input_homepage"));
+        searchInputBox.clear();
+        searchInputBox.sendKeys("w pustyni i w puszczy");
+        searchInputBox.submit();
 //        driver.quit();
     }
 }
