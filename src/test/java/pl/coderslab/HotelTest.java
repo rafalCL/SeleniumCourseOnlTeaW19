552b3503e7e9c998eabcee26bf7259bf0235d031
Rafal <rafal.opial@coderslab.pl>
2022-02-27 15:43:26 +0100

    timeouts

--- src/test/java/pl/coderslab/HotelTest.java
+++ src/test/java/pl/coderslab/HotelTest.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class HotelTest {
     @Test
     public void shouldSearchWithDuckDuckGo() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
         driver.get("https://hotel-testlab.coderslab.pl/en/");
         WebElement hotelLocationInput = driver.findElement(By.id("hotel_location"));
         WebElementUtil.setText("Warsaw", hotelLocationInput);
