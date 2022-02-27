50855ff3e0b801d782b7383e3fb8db38b983ccbd
Rafal <rafal.opial@coderslab.pl>
2022-02-27 16:21:45 +0100

    test red borders

--- src/test/java/pl/coderslab/HotelTest.java
+++ src/test/java/pl/coderslab/HotelTest.java
@@ -1,5 +1,6 @@
 package pl.coderslab;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,18 +8,41 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HotelTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void beforeEach() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        this.driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
+    }
+
     @Test
     public void shouldSearchWithDuckDuckGo() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
-        driver.get("https://hotel-testlab.coderslab.pl/en/");
+        this.driver.get("https://hotel-testlab.coderslab.pl/en/");
         WebElement hotelLocationInput = driver.findElement(By.id("hotel_location"));
         WebElementUtil.setText("Warsaw", hotelLocationInput);
         WebElement newsletterEmailInput = driver.findElement(By.id("newsletter-input"));
         WebElementUtil.setText("test@test.com", newsletterEmailInput);
 //        driver.quit();
+    }
+
+    @Test
+    public void shouldAddErrorBorderToEmptyFormElements() {
+        // given
+        final String pageUrl = "https://hotel-testlab.coderslab.pl/en/";
+        driver.get(pageUrl);
+        // when
+        WebElement submitButton = driver.findElement(By.id("search_room_submit"));
+        submitButton.click();
+        // then
+        assertEquals(pageUrl, driver.getCurrentUrl());
+        List<WebElement> errorBorderElements = driver.findElements(By.cssSelector("#search_hotel_block_form .error_border"));
+        assertEquals(4, errorBorderElements.size());
     }
 }
