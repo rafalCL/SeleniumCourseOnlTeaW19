cd12a653eb2dcff916c1e61f7909ed806b0198ab
Rafal <rafal.opial@coderslab.pl>
2022-03-12 10:35:37 +0100

    refactor test to use main page page object

--- src/test/java/pl/coderslab/pageobject/HotelTest.java
+++ src/test/java/pl/coderslab/pageobject/HotelTest.java
@@ -2,9 +2,7 @@ package pl.coderslab.pageobject;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -18,14 +16,11 @@ public class HotelTest {
     @Test
     public void shouldSubscribeToNotifications() {
         this.driver.get("https://hotel-testlab.coderslab.pl/en/");
-        WebElement newsletterInput = driver.findElement(By.id("newsletter-input"));
-        WebElement submitNewsletter = driver.findElement(By.name("submitNewsletter"));
-        newsletterInput.clear();
+        HotelMainPage mainPage = new HotelMainPage(driver);
         String randomEmail = UUID.randomUUID().toString() + "@email.pl";
-        newsletterInput.sendKeys(randomEmail);
-        submitNewsletter.click();
-        WebElement successPanel = driver.findElement(By.cssSelector(".alert.alert-success"));
-        assertEquals("Newsletter : You have successfully subscribed to this newsletter.", successPanel.getText());
+        mainPage.subscribeToNotifications(randomEmail);
+        String successText = mainPage.getSuccessText();
+        assertEquals("Newsletter : You have successfully subscribed to this newsletter.", successText);
     }
 
     @BeforeEach
