7af205743513b7efa02df94f7c46da45f5b2fddb
Rafal <rafal.opial@coderslab.pl>
2022-03-13 12:22:00 +0100

    implement fourth scenario step

--- src/test/java/pl/coderslab/cucumberexample/DuckDuckGoTextSearchSteps.java
+++ src/test/java/pl/coderslab/cucumberexample/DuckDuckGoTextSearchSteps.java
@@ -2,12 +2,16 @@ package pl.coderslab.cucumberexample;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class DuckDuckGoTextSearchSteps {
     private WebDriver driver;
@@ -18,8 +22,6 @@ public class DuckDuckGoTextSearchSteps {
         this.driver = new ChromeDriver();
         this.driver.get("https://duckduckgo.com/");
     }
-
-//        searchInputBox.submit();
 
     @When("^Search phrase: \"W pustyni i w puszczy\" entered in search input box$")
     public void searchForPhrase() {
@@ -34,5 +36,16 @@ public class DuckDuckGoTextSearchSteps {
         searchInputBox.sendKeys(Keys.ENTER);
     }
 
-//    Then First 3 search results contain phrase: "W pustyni i w puszczy"
+    @Then("^First 3 search results contain phrase: \"W pustyni i w puszczy\"$")
+    public void firstResultsContainPhrase() {
+        List<WebElement> searchResultsLinks = driver.findElements(By.className("result__a"));
+        final String requiredPhrase = "W pustyni i w puszczy";
+        for (int i = 0; i < 3; i++) {
+            String linkText = searchResultsLinks.get(i).getText();
+            if (!linkText.toLowerCase()
+                    .contains(requiredPhrase.toLowerCase())) {
+                Assert.fail(String.format("Result should contain (case insensitive): %s, but was: %s", requiredPhrase, linkText));
+            }
+        }
+    }
 }
