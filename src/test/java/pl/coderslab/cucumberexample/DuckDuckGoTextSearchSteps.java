e84108ac1624d7a57a1660a14f08e32d8809dc67
Rafal <rafal.opial@coderslab.pl>
2022-03-13 11:58:04 +0100

    implement first scenario step

---
+++ src/test/java/pl/coderslab/cucumberexample/DuckDuckGoTextSearchSteps.java
@@ -0,0 +1,27 @@
+package pl.coderslab.cucumberexample;
+
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DuckDuckGoTextSearchSteps {
+    private WebDriver driver;
+
+    @Given("^Page https://duckduckgo.com/ opened in browser$")
+    public void pageOpenedInBrowser() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        this.driver = new ChromeDriver();
+        this.driver.get("https://duckduckgo.com/");
+    }
+
+    //        WebElement searchInputBox = driver.findElement(By.id("search_form_input_homepage"));
+//        searchInputBox.clear();
+//        searchInputBox.sendKeys("w pustyni i w puszczy");
+//        searchInputBox.submit();
+
+//    When Search phrase: "W pustyni i w puszczy" entered in search input box
+//    And Key Enter pressed
+//    Then First 3 search results contain phrase: "W pustyni i w puszczy"
+}
