88949f147d6049489ac54e46d64f35708b2c246a
Rafal <rafal.opial@coderslab.pl>
2022-03-13 12:02:25 +0100

    implement third scenario step

--- src/test/java/pl/coderslab/cucumberexample/DuckDuckGoTextSearchSteps.java
+++ src/test/java/pl/coderslab/cucumberexample/DuckDuckGoTextSearchSteps.java
@@ -1,8 +1,10 @@
 package pl.coderslab.cucumberexample;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -25,6 +27,12 @@ public class DuckDuckGoTextSearchSteps {
         searchInputBox.clear();
         searchInputBox.sendKeys("w pustyni i w puszczy");
     }
-    //    And Key Enter pressed
+
+    @And("^Key Enter pressed$")
+    public void pressEnterInSearchInput() {
+        WebElement searchInputBox = driver.findElement(By.id("search_form_input_homepage"));
+        searchInputBox.sendKeys(Keys.ENTER);
+    }
+
 //    Then First 3 search results contain phrase: "W pustyni i w puszczy"
 }
