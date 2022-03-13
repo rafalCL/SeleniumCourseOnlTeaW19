5531ff0aaa631be11fd5a23d956866af0caf8c66
Rafal <rafal.opial@coderslab.pl>
2022-03-13 15:32:07 +0100

    add DDG page object and refactor Steps class to use it

---
+++ src/test/java/pl/coderslab/cucumberexample/DuckDuckGoMainPage.java
@@ -0,0 +1,42 @@
+package pl.coderslab.cucumberexample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuckDuckGoMainPage {
+    private WebDriver driver;
+    @FindBy(id = "search_form_input_homepage")
+    private WebElement searchInputBox;
+
+    public DuckDuckGoMainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public void enterSearchPhrase(String phrase) {
+        searchInputBox.clear();
+        searchInputBox.sendKeys(phrase);
+    }
+
+    public void submitSearchPhrase() {
+        searchInputBox.sendKeys(Keys.ENTER);
+    }
+
+    public List<String> getSearchResultsLinksTexts() {
+        List<String> result = new ArrayList<>();
+        List<WebElement> searchResultsLinks = driver.findElements(By.className("result__a"));
+
+        for (WebElement thisLink : searchResultsLinks) {
+            result.add(thisLink.getText());
+        }
+
+        return result;
+    }
+}
--- src/test/java/pl/coderslab/cucumberexample/DuckDuckGoTextSearchSteps.java
+++ src/test/java/pl/coderslab/cucumberexample/DuckDuckGoTextSearchSteps.java
@@ -5,42 +5,39 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
 public class DuckDuckGoTextSearchSteps {
     private WebDriver driver;
+    private DuckDuckGoMainPage ddgMainPage;
 
     @Given("^Page (.*) opened in browser$")
     public void pageOpenedInBrowser(String pageUrl) {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         this.driver = new ChromeDriver();
         this.driver.get(pageUrl);
+        this.ddgMainPage = new DuckDuckGoMainPage(driver);
     }
 
     @When("^Search phrase: (.*) entered in search input box$")
     public void searchForPhrase(String phrase) {
-        WebElement searchInputBox = driver.findElement(By.id("search_form_input_homepage"));
-        searchInputBox.clear();
-        searchInputBox.sendKeys(phrase);
+        ddgMainPage.enterSearchPhrase(phrase);
     }
 
     @And("^Key Enter pressed$")
     public void pressEnterInSearchInput() {
-        WebElement searchInputBox = driver.findElement(By.id("search_form_input_homepage"));
-        searchInputBox.sendKeys(Keys.ENTER);
+        ddgMainPage.submitSearchPhrase();
     }
 
     @Then("^First (.*) search results contain phrase: (.*)$")
     public void firstResultsContainPhrase(int count, String requiredPhrase) {
-        List<WebElement> searchResultsLinks = driver.findElements(By.className("result__a"));
+        List<String> searchResultsLinksTexts = ddgMainPage.getSearchResultsLinksTexts();
+
         for (int i = 0; i < count; i++) {
-            String linkText = searchResultsLinks.get(i).getText();
+            String linkText = searchResultsLinksTexts.get(i);
             if (!linkText.toLowerCase()
                     .contains(requiredPhrase.toLowerCase())) {
                 Assert.fail(String.format("Result should contain (case insensitive): %s, but was: %s", requiredPhrase, linkText));
